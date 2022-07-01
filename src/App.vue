25182854b1783883541ac3571c985a107266e1cc
rddavino <rd.davino@gmail.com>
2022-07-01 12:41:29 -0300

    refactor: alteração da fonte para open sans

--- src/App.vue
+++ src/App.vue
@@ -11,11 +11,11 @@ export default {
 </script>
 
 <style>
-@import url('https://fonts.googleapis.com/css2?family=Gantari:wght@200;300;400;500&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;500&display=swap');
 
 body {
   margin: 0 auto;
-  font-family: 'Gantari', sans-serif;
+  font-family: 'Open Sans', sans-serif;
 
 }
 </style>
